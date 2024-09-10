397afa752c3621cb90444c9294eb653b5540eaf5
MALB1993 <mostafaalaeddin570@gmail.com>
2024-09-09 20:10:24 -1200

    initial project with bootstrap vite

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { resolve } from 'path'
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
-});
+export default {
+  root: resolve(__dirname, 'src'),
+  build: {
+    outDir: '../dist'
+  },
+  server: {
+    port: 8080
+  }
+}
