21037fe8307478af2596ae82ca0e4289e264a199
MALB1993 <mostafaalaeddin570@gmail.com>
2024-09-15 22:48:11 -1200

    fixed admin template

--- NovoDecor/routes/web.php
+++ NovoDecor/routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('index');
+});
+
+
+Route::prefix('/admin-managements')->name('admin')->controller(AdminController::class)->group(function(){
+    Route::get('/', 'index')->name('index');
 });
